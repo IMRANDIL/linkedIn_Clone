f9125077933e093fc5f9b2c24711b56a78ece0d4
IMRANDIL <aliimranadil2@gmail.com>
2023-02-20 10:27:12 +0530

    added some more change

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,17 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const Routes = () => {
   return (
-    <View>
-      <Text>Routes</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Ho} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
