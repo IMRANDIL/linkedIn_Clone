ec188996479148d31a4bb7c05945ff87896e280e
IMRANDIL <aliimranadil2@gmail.com>
2023-02-20 10:21:51 +0530

    some more

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Routes from './src/Routes';
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
+  return <Routes />;
 };
 
 export default App;
